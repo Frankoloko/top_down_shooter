718c87fcbb75c449090e8b666e52e1e3093b7587
Francois Kruger <francois.kruger@triggerfish.co.za>
2021-01-27 06:51:13 +0200

    Added an enemy with HP and the bullet now kills the enemies

---
+++ Top Down Shooter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Vector2 velocity = new Vector2(0.0f, 0.0f);
+    public GameObject shooter;
+
+    void Update() {
+        // Get the bullet's current position and new position (next frame position)
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 newPosition = currentPosition + velocity * Time.deltaTime;
+
+        // Cast a ray between the current position and the new position, and check if there are any objects it runs in to
+        RaycastHit2D[] hits = Physics2D.LinecastAll(currentPosition, newPosition);
+
+        foreach(RaycastHit2D hit in hits) {
+            GameObject other = hit.collider.gameObject;
+            if (other != shooter) {
+                if (other.CompareTag("Enemy")) {
+                    Destroy(gameObject);
+                    Debug.Log(other.name);
+                    GameObject enemyObject = GameObject.Find(other.name);
+                    EnemyRed enemy = enemyObject.GetComponent<EnemyRed>();
+                    if (enemy.health == 1) {
+                        print("DEAD!");
+                        Destroy(enemyObject);
+                        break;
+                    }
+                    enemy.health = enemy.health - 1;
+                    break;
+                }
+
+                if (other.CompareTag("Prop")) {
+                    Destroy(gameObject);
+                    Debug.Log(other.name);
+                    break;
+                }
+            }
+        }
+
+        transform.position = newPosition;
+    }
+}
---
+++ Top Down Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Objects")]
+    public Rigidbody2D rigidbody;
+    public Animator animator;
+    public GameObject ProjectilePrefab;
+
+    [Space]
+    [Header("Settings")]
+    public float movementSpeed = 7f;
+    public float ProjectileSpeed = 18f;
+    public float DestroyTime = 5f;
+
+    [Space]
+    [Header("Watchers")]
+    public Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        ShootProjectile();
+    }
+
+    void FixedUpdate()
+    {
+       rigidbody.MovePosition(rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    void MovePlayer()
+    {
+        // Get movement direction value
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // This IF stops the animation to go back to the "default DOWN" spite. Since the Speed is left at > 0 (the previous frame) the spite will be left at what it last was
+        if (!(movement.x == 0 & movement.y == 0)) {
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+        }
+    }
+
+    void ShootProjectile()
+    {
+        // Shooting code
+        if (Input.GetKeyDown("left") ^ Input.GetKeyDown("right") ^ Input.GetKeyDown("down") ^ Input.GetKeyDown("up")) {
+            // Get the velocity (direction)
+            Vector2 velocity = new Vector2(0.0f, 0.0f);
+            if (Input.GetKeyDown("left")) {
+                velocity = new Vector2(ProjectileSpeed * -1, 0.0f);
+            }
+            if (Input.GetKeyDown("right")) {
+                velocity = new Vector2(ProjectileSpeed, 0.0f);
+            }
+            if (Input.GetKeyDown("up")) {
+                velocity = new Vector2(0.0f, ProjectileSpeed);
+            }
+            if (Input.GetKeyDown("down")) {
+                velocity = new Vector2(0.0f, ProjectileSpeed * -1);
+            }
+
+            // Create new projectile
+            GameObject projectile = Instantiate(ProjectilePrefab, transform.position, Quaternion.identity);
+            Bullet bullet = projectile.GetComponent<Bullet>();
+            // Give new projective velocity
+            bullet.velocity = velocity;
+            bullet.shooter = gameObject;
+            // Destory the new projectile after an X amount of time
+            Destroy(projectile, DestroyTime);
+        }
+    }
+}
